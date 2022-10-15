f56d4e2f2c55198bca2a40ce0f4358357fc51220
Chen Junda <ddadaal@outlook.com>
2022-10-15 10:46:16 +0800

    feat: add more react rules

---
+++ example/react.tsx
@@ -0,0 +1,21 @@
+export const A = () => {
+
+  const a = (
+    <div>
+
+    </div>
+  );
+
+  return (
+
+    <div
+      style={{}}
+      className=""
+      test="1"
+    >
+      <p test="1"></p>
+      <p test="1"></p>
+      {a}
+    </div>
+  );
+};
